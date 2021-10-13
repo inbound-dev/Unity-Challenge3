fe68c817f1d210fa12db1e2f680c92f946e9478d
Jordan Hamilton <jordanh6489@hotmail.com>
2021-10-13 14:36:37 -0400

    Debugging

--- Unity-Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Unity-Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -24,8 +24,12 @@ public class PlayerControllerX : MonoBehaviour
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
 
+        playerRb = GetComponent<Rigidbody>();
+
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+        gameOver = false;
 
     }
 
@@ -35,7 +39,7 @@ public class PlayerControllerX : MonoBehaviour
         // While space is pressed and player is low enough, float up
         if (Input.GetKey(KeyCode.Space) && !gameOver)
         {
-            playerRb.AddForce(Vector3.up * floatForce);
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
         }
     }
 
